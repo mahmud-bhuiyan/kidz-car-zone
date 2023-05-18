0a007961504cd1a2a054d3769835f74e32d4bf6b
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-05-19 02:06:18 +0600

    navbar added

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer/Footer";
+import Navbar from "../Pages/Shared/Navbar/Navbar";
 
 const MainLayout = () => {
   return (
     <div className="max-w-7xl mx-auto">
+      <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
---
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,98 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const navItems = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/">All Toys</Link>
+      </li>
+      <li>
+        <Link to="/">My Toys</Link>
+      </li>
+      <li>
+        <Link to="/">Add A Toy</Link>
+      </li>
+      <li>
+        <Link to="/blog">Blog</Link>
+      </li>
+      <li>
+        <Link to="/register">Sign Up</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </>
+  );
+  return (
+    <>
+      <div className="navbar bg-base-100 h-28">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li>
+                <a>Item 2</a>
+              </li>
+              <li>
+                <a>Item 3</a>
+              </li>
+            </ul>
+          </div>
+
+          <Link to="/" className="flex items-center">
+            <img
+              className="h-12 w-12 mr-4 rounded"
+              src="https://i.ibb.co/zbHzfxB/kcz-svg.jpg"
+              alt="KidzCarZone Logo"
+            />
+            <p className="text-xl font-bold hidden lg:flex">KidzCarZone</p>
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{navItems}</ul>
+        </div>
+        <div className="navbar-end">
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src="" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {navItems}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
