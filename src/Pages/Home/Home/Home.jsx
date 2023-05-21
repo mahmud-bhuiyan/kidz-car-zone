977525eac10e3c30c7e9629ffe57780ac9184707
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-05-21 20:58:25 +0600

    aos implemented

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,15 +1,31 @@
+import { useEffect } from "react";
 import useTitle from "../../../Hooks/useTitle";
 import Banner from "../Banner/Banner";
 import Gallery from "../Gallery/Gallery";
 import ShopByCategory from "../ShopByCategory/ShopByCategory";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Home = () => {
   useTitle("Home");
+
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
   return (
     <div>
-      <Banner></Banner>
-      <Gallery></Gallery>
-      <ShopByCategory></ShopByCategory>
+      <div data-aos="fade-up">
+        <Banner></Banner>
+      </div>
+
+      <div data-aos="fade-up">
+        <Gallery></Gallery>
+      </div>
+
+      <div data-aos="fade-up">
+        <ShopByCategory></ShopByCategory>
+      </div>
     </div>
   );
 };
